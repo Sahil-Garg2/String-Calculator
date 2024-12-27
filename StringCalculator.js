0425d96f73647d38ff47434f69bee3409e743de4
Sahil Garg <43277712+Sahil-Garg2@users.noreply.github.com>
2024-12-27 07:55:01 +0530

    Built(calculator): Handled multple number with comma separated strings

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,9 +1,11 @@
 function add(numbers) {
   if (!numbers) return 0;
-  return parseInt(numbers);
+  return numbers.split(",").reduce((sum, num) => sum + parseInt(num), 0);
 }
 
 // Test Case
 console.assert(add("") === 0, "Test Failed: Empty string should return 0");
 console.assert(add("1") === 1, "Test Failed: '1' should return 1");
 console.assert(add("5") === 5, "Test Failed: '5' should return 5");
+console.assert(add("1,2") === 3, "Test Failed: '1,2' should return 3");
+console.assert(add("4,5") === 9, "Test Failed: '4,5' should return 9");
