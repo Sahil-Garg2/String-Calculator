1d6b4a073a7903ba68f3686a9865798d6519df31
Sahil Garg <43277712+Sahil-Garg2@users.noreply.github.com>
2024-12-27 07:53:40 +0530

    build(calculator): Added more TC for single digit

--- StringCalculator.js
+++ StringCalculator.js
@@ -5,3 +5,5 @@ function add(numbers) {
 
 // Test Case
 console.assert(add("") === 0, "Test Failed: Empty string should return 0");
+console.assert(add("1") === 1, "Test Failed: '1' should return 1");
+console.assert(add("5") === 5, "Test Failed: '5' should return 5");
